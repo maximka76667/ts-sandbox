e89189edc5a0fab5fad9825d1ca996e3e3fbd265
Maxim <74974283+maximka76667@users.noreply.github.com>
2022-02-14 10:48:17 +0100

    exercise 2 solution

--- src/Exercise_2_Functions/exercise2.ts
+++ src/Exercise_2_Functions/exercise2.ts
@@ -14,15 +14,15 @@ export default () => {
   // • Add explicit parameter types and return type
   // • Fix any errors resulting from invalid types
 
-  function add(x, y) {
+  function add(x: number, y: number): number {
     return x + y;
   }
 
-  function sumArray(numbers) {
+  function sumArray(numbers: number[]) {
     return numbers.reduce(add, 0);
   }
 
-  const someSum = sumArray(['3', '6', '9']);
+  const someSum = sumArray([3, 6, 9]);
 
   console.log('[Exercise 2.1]', `3 + 6 + 9 === ${someSum}`);
 
@@ -31,13 +31,16 @@ export default () => {
   // • Add explicit parameter types and return types to the `deposit` function
   // • Make the function's `message` parameter *optional*
 
-  const bankAccount = {
+  interface BankAccount {
+    money: number;
+    deposit: (value: number, message?: string) => void;
+  }
+
+  const bankAccount: BankAccount = {
     money: 0,
-    deposit(value, message) {
+    deposit(value: number, message?: string): void {
       this.money += value;
-      if (message) {
-        console.log(message);
-      }
+      if (message) console.log(message);
     }
   };
 
@@ -51,13 +54,13 @@ export default () => {
   // Instructions:
   // • Add type annotations wherever possible
 
-  function computeScore(word) {
+  function computeScore(word: string) {
     const letters = word.toUpperCase().split('');
-    return letters.reduce((accum, curr) => accum += getPointsFor(curr), 0);
+    return letters.reduce((accum: number, curr: string) => accum += getPointsFor(curr), 0);
   }
 
-  function getPointsFor(letter) {
-    const lettersAndPoints = [
+  function getPointsFor(letter: string): number {
+    const lettersAndPoints: [string, number][] = [
       ['AEOIULNRST', 1],
       ['DG', 2],
       ['BCMP', 3],
@@ -67,9 +70,9 @@ export default () => {
       ['QZ', 10],
     ];
 
-    return lettersAndPoints.reduce((computedScore, pointsTuple) => {
-      const [letters, score] = pointsTuple;
-      if (letters.split('').find((ll) => ll === letter)) {
+    return lettersAndPoints.reduce((computedScore: number, pointsTuple: [string, number]) => {
+      const [letters, score]: [string, number] = pointsTuple;
+      if (letters.split('').find((ll: string) => ll === letter)) {
         return computedScore += score;
       }
       return computedScore;
@@ -83,7 +86,7 @@ export default () => {
   // • Add explicit parameter types and return types
   // • Add a default greeting: "hello"
 
-  function greet(greeting) {
+  function greet(greeting: string = "Hello") {
     return greeting.toUpperCase();
   }
 
@@ -97,11 +100,11 @@ export default () => {
   // • Add parameter type annotation
   // • Even though this function doesn't return, add an explicit return type
 
-  function layEggs(quantity, color) {
+  function layEggs(quantity: number, color: string): void {
     console.log(`[Exercise 2.5] You just laid ${quantity} ${color} eggs. Good job!`);
   }
 
-  layEggs();
+  layEggs(10, 'red');
 
   // ======== Exercise 2.6 ========
   // Here we've initialized two variables with function types.
@@ -112,11 +115,11 @@ export default () => {
   let multiply: (val1: number, val2: number) => number;
   let capitalize: (val: string) => string;
 
-  multiply = function(value: string): string {
+  capitalize = function(value: string): string {
     return `${value.charAt(0).toUpperCase()}${value.slice(1)}`;
   }
 
-  capitalize = function(x: number, y: number): number {
+  multiply = function(x: number, y: number): number {
     return x * y;
   }
 
@@ -142,19 +145,19 @@ export default () => {
   const numberCollection: number[] = [];
   const stringCollection: string[] = [];
   
-  function pushToCollection(item, collection) {
+  function pushToCollection<Type>(item: Type, collection: Type[]): Type[] {
     collection.push(item);
     return collection;
   }
   
   // Add some stuff to the collections
-  pushToCollection(false, stringCollection);
+  pushToCollection('false', stringCollection);
   pushToCollection('hi', stringCollection);
-  pushToCollection([], stringCollection);
+  pushToCollection('[]', stringCollection);
 
-  pushToCollection('1', numberCollection);
-  pushToCollection('2', numberCollection);
-  pushToCollection('3', numberCollection);
+  pushToCollection(1, numberCollection);
+  pushToCollection(2, numberCollection);
+  pushToCollection(3, numberCollection);
 
   const incrementedByTwo = numberCollection.map((num) => num + 2);
   
